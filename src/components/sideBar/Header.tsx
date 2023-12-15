a9c9e56703358d649149f197566cf15480f2909b
Rahul R Yadav <rryadav2025@gmail.com>
2023-12-15 16:21:17 +0545

    nav bgcolor changable, logo & navitems editable

--- src/components/sideBar/Header.tsx
+++ src/components/sideBar/Header.tsx
@@ -1,108 +1,275 @@
-import CustomButton from "../common/CustomButton"
-import Select, { SelectChangeEvent } from '@mui/material/Select'
-import { Typography } from "@mui/material"
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import { useState } from "react"
-import TextField from '@mui/material/TextField'
-import { HexColorPicker } from "react-colorful"
-import InputAdornment from '@mui/material/InputAdornment'
-import deserialize from "../../utils/deserialize"
-import basic from "../../data/header/nav-basic.json"
-import centered from "../../data/header/nav-centered.json"
+import CustomButton from "../common/CustomButton";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { Typography } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import { HexColorPicker } from "react-colorful";
+import InputAdornment from "@mui/material/InputAdornment";
+import deserialize from "../../utils/deserialize";
+import basic from "../../data/header/nav-basic.json";
+import centered from "../../data/header/nav-centered.json";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import rgbToHex from "../../utils/rgbToHex";
+import { AddCardOutlined } from "@mui/icons-material";
+import TextDialog from "../common/TextDialog";
 
 const Header = () => {
-    const [style, setStyle] = useState('Basic');
-    const [bgColor, setBgColor] = useState("#121212");
-    const [textColor, setTextColor] = useState("#121212");
+  const [style, setStyle] = useState("Basic");
+  const [bgColor, setBgColor] = useState("");
+  const [text, setText] = useState("");
+  const [textColor, setTextColor] = useState("");
+  const [image, setImage] = useState("");
 
-    let header;
+  let header;
 
-    if (style === 'Basic') {
-        header = deserialize(basic);
-    } else if (style === 'Centered') {
-        header = deserialize(centered);
+  if (style === "Basic") {
+    header = deserialize(basic);
+  } else if (style === "Centered") {
+    header = deserialize(centered);
+  }
+
+  function cloudinary(image: any) {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "dynamic-cms");
+    data.append("cloud_name", "dssvqu4bj");
+
+    fetch("https://api.cloudinary.com/v1_1/dssvqu4bj/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data.url);
+        console.log("success", data.url);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  }
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImage(reader.result as string);
+      cloudinary(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
     }
+  };
 
-    return (
+  useEffect(() => {
+    const Logo = document.getElementById("logo") as HTMLImageElement;
+    Logo.src = image;
+  }, [image]);
+
+  useEffect(() => {
+    if (bgColor == "") {
+      setBgColor(
+        rgbToHex(
+          document.getElementById("navBackground")!.style.backgroundColor
+        )
+      );
+    } else {
+      document.getElementById("navBackground")!.style.backgroundColor = bgColor;
+    }
+  }, [bgColor]);
+
+  //   useEffect(() => {
+  //     if (textColor == "") {
+  //       setTextColor(rgbToHex(document.getElementById("navItems")!.style.color));
+  //       setText(document.getElementById("navItems")!.innerHTML);
+  //     } else {
+  //       document.getElementById("navItems")!.style.color = textColor;
+  //       document.getElementById("navItems")!.innerText = text;
+  //     }
+  //   }, [textColor, text]);
+
+  const initialDetail = {
+    navHeader: "",
+    navHome: "",
+    navAbout: "",
+    navServices: "",
+    navContact: "",
+  } as { [key: string]: string };
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const [detail, setDetail] = useState(initialDetail);
+
+  useEffect(() => {
+    setDetail(initialDetail);
+    setValue("");
+  }, [style]);
+
+  useEffect(() => {
+    Object.keys(detail).forEach((item: string) => {
+      if (detail[item] == "") {
+        setDetail({
+          ...detail,
+          [item]: document.getElementById(item)?.innerHTML as string,
+        });
+      } else {
+        document.getElementById(item)!.innerHTML = detail[item];
+      }
+    });
+  }, [detail]);
+
+  function handleClick(id: string) {
+    setValue(id);
+    setOpen(true);
+  }
+
+  const addClickListener = (id: string, handleClick: (id: string) => void) => {
+    const element = document.getElementById(id);
+    element?.addEventListener("click", () => handleClick(id));
+  };
+
+  addClickListener("navHeader", handleClick);
+  addClickListener("navHome", handleClick);
+  addClickListener("navAbout", handleClick);
+  addClickListener("navServices", handleClick);
+  addClickListener("navContact", handleClick);
+
+  return (
+    <div>
+      <p className="font-gilroy-bold text-gray-400">NAVIGATION BAR EDITOR</p>
+
+      <div className="my-4">
+        <Typography variant="subtitle1">Navigation Bar Style</Typography>
+        <div className="w-1/4 my-1">
+          <FormControl size="small" fullWidth>
+            <Select
+              value={style}
+              onChange={(e: SelectChangeEvent) => setStyle(e.target.value)}
+            >
+              <MenuItem value="Basic">Basic</MenuItem>
+              <MenuItem value="Centered">Centered</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+
+      <div className="flex my-4 justify-between w-[75%]">
         <div>
-            <p className="font-gilroy-bold text-gray-400">NAVIGATION BAR EDITOR</p>
+          <Typography variant="subtitle1">Background Color</Typography>
+          <TextField
+            variant="outlined"
+            size="small"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <div
+                    className="rounded-full w-4 h-4"
+                    style={{
+                      backgroundColor: bgColor,
+                    }}
+                  ></div>
+                </InputAdornment>
+              ),
+            }}
+            sx={{ marginBottom: "0.5rem" }}
+            value={bgColor}
+            onChange={(e) => setBgColor(e.target.value)}
+          />
+          <HexColorPicker color={bgColor} onChange={setBgColor} />
+        </div>
+        <div>
+          <Typography variant="subtitle1">Text Color</Typography>
 
-            <div className="my-4">
-                <Typography variant="subtitle1">Navigation Bar Style</Typography>
-                <div className="w-1/4 my-1">
-                    <FormControl size="small" fullWidth>
-                        <Select
-                            value={style}
-                            onChange={(e: SelectChangeEvent) => setStyle(e.target.value)}
-                        >
-                            <MenuItem value="Basic">Basic</MenuItem>
-                            <MenuItem value="Centered">Centered</MenuItem>
-                        </Select>
-                    </FormControl>
-                </div>
-            </div>
+          <TextField
+            variant="outlined"
+            size="small"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <div
+                    className="rounded-full w-4 h-4"
+                    style={{
+                      backgroundColor: textColor,
+                    }}
+                  ></div>
+                </InputAdornment>
+              ),
+            }}
+            value={textColor}
+            onChange={(e) => setTextColor(e.target.value)}
+            sx={{ marginBottom: "0.5rem" }}
+          />
+          <HexColorPicker color={textColor} onChange={setTextColor} />
+        </div>
 
-            <div className="flex my-4 justify-between w-[45%]">
-                <div>
-                    <Typography variant="subtitle1">Background Color</Typography>
-                    <TextField
-                        variant="outlined"
-                        size="small"
-                        InputProps={{
-                            startAdornment: <InputAdornment position="start">
-                                <div className="rounded-full w-4 h-4" style={{
-                                    backgroundColor: bgColor
-                                }}>
+        <div>
+          <Typography>Upload Image</Typography>
+          <div
+            className="w-64 h-40 border-2 mt-2 border-dashed flex items-center cursor-pointer rounded justify-center"
+            onClick={() => document.getElementById("image")?.click()}
+          >
+            <input
+              type="file"
+              id="image"
+              accept="image/png, image/jpeg, image/webp"
+              hidden
+              onChange={(event) => {
+                const { files } = event.target;
+                if (files) {
+                  handleImageChange(event);
+                }
+                event.target.value = "";
+              }}
+            />
+            {image ? (
+              <div className="w-full h-full relative flex items-center justify-center">
+                <img
+                  src={image}
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "100%",
+                    objectFit: "contain",
+                  }}
+                />
+              </div>
+            ) : (
+              <div className="flex flex-col justify-center items-center">
+                <CloudUploadIcon sx={{ fontSize: 35 }} />
+                <p className="font-gilroy-regular text-xs">
+                  Supported file types: <b>JPG, PNG, WEBP</b>
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
 
-                                </div>
-                            </InputAdornment>,
-                        }}
-                        sx={{ marginBottom: '0.5rem' }}
-                        value={bgColor}
-                        onChange={(e) => setBgColor(e.target.value)}
-                    />
-                    <HexColorPicker
-                        color={bgColor}
-                        onChange={setBgColor}
-                    />
-                </div>
-                <div>
-                    <Typography variant="subtitle1">Text Color</Typography>
+      <Typography variant="subtitle1">Preview</Typography>
 
-                    <TextField
-                        variant="outlined"
-                        size="small"
-                        InputProps={{
-                            startAdornment: <InputAdornment position="start">
-                                <div className="rounded-full w-4 h-4" style={{
-                                    backgroundColor: textColor
-                                }}>
+      <div
+        className="my-6 h-14 scale-95"
+        style={{
+          marginLeft: "-3%",
+        }}
+      >
+        {header}
+      </div>
 
-                                </div>
-                            </InputAdornment>,
-                        }}
-                        value={textColor}
-                        onChange={(e) => setTextColor(e.target.value)}
-                        sx={{ marginBottom: '0.5rem' }}
-                    />
-                    <HexColorPicker
-                        color={textColor}
-                        onChange={setTextColor}
-                    />
-                </div>
-            </div >
-            <Typography variant="subtitle1">Preview</Typography>
+      <TextDialog
+        open={open}
+        handleTextChange={(e) =>
+          setDetail({ ...detail, [value]: e.target.value })
+        }
+        handleClose={() => setOpen(false)}
+        value={value}
+        field={detail}
+      />
 
-            <div className="my-6 h-14 scale-95" style={{
-                "marginLeft": "-3%",
-            }}>
-                {header}
-            </div>
+      <CustomButton buttonText="Save Changes" />
+    </div>
+  );
+};
 
-            <CustomButton buttonText="Save Changes" />
-        </div >
-    )
-}
-
-export default Header
+export default Header;
